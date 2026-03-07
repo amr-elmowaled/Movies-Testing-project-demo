0b0097ce6e9b35d0644192bb4d1a9e3c107b35c5
Amr Elmowaled <amrelmowaled@Amrs-MacBook-Pro.local>
2026-03-07 18:58:18 +0200

    initial commit

---
+++ src/main/FileManager.java
@@ -0,0 +1,19 @@
+package main;
+
+public class FileManager {
+
+    public static String readUserFile(String path) {
+
+        return null;
+    }
+
+    public static String readMoviesFile(String path) {
+
+        return null;
+    }
+
+    public static void writeRecommendationFile(String path, String content) {
+
+    }
+
+}
---
+++ src/main/Validatable.java
@@ -0,0 +1,6 @@
+package main;
+
+public interface Validatable {
+
+    public boolean isValidParams();
+}
